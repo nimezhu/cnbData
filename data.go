dcc48520c216a511369e1988a3106a4dabb7e505
zhuxp <nimezhu@gmail.com>
2018-06-21 11:55:56 -0400

    add header authorization

--- data.go
+++ data.go
@@ -13,6 +13,7 @@ func cred(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
